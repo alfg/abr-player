77e57b8e0ddc20755ab5c26bbfb29d90b1852938
Alf <alf.g.jr@gmail.com>
2019-03-26 21:54:24 -0700

    revamped probe.vue a bit.

--- src/components/Probe.vue
+++ src/components/Probe.vue
@@ -2,90 +2,22 @@
   <div class="probe">
     <h4>Browser Compatibility</h4>
     <div class="row">
-      <div class="two columns">
+      <div class="three columns">
       <ul>
-        <li>
-          <span class="material-icons checked" v-if="player === 'ShakaPlayer'">check_circle</span>
+        <li v-for="(value, key) in supportedDrms" :key="key">
+          <span class="material-icons checked" v-if="value !== null">check_circle</span>
           <span class="material-icons" v-else>close</span>
-          Shaka Player
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="player === 'HasPlayer'">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          HasPlayer
+          {{key}}
         </li>
       </ul>
       </div>
 
-      <div class="two columns">
-      <ul>
-        <li>
-          <span class="material-icons checked" v-if="supportsDASH">check_circle</span>
+      <div class="nine columns">
+      <ul class="codecs">
+        <li v-for="(value, key) in supportedMedia" :key="key">
+          <span class="material-icons checked" v-if="value">check_circle</span>
           <span class="material-icons" v-else>close</span>
-          DASH
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="supportsMSS">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          MSS
-        <li>
-          <span class="material-icons checked" v-if="supportsHLS">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          HLS
-        </li>
-      </ul>
-      </div>
-
-      <div class="two columns">
-      <ul>
-        <li>
-          <span class="material-icons checked" v-if="supportsWidevine">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          widevine
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="supportsPlayready">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          playready
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="supportsFairplay">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          fairplay
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="supportsPrimetime">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          primetime
-        </li>
-      </ul>
-      </div>
-
-      <div class="six columns">
-      <ul>
-        <li>
-          <span class="material-icons checked" v-if="supportsMp4Avc1">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          mp4/avc1
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="supportsMp4Hevc">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          mp4/hevc
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="supportsMp4Ec3">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          mp4/ec-3</li>
-        <li>
-          <span class="material-icons checked" v-if="supportsWebmVp8">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          webm/vp8
-        </li>
-        <li>
-          <span class="material-icons checked" v-if="supportsWebmVp9">check_circle</span>
-          <span class="material-icons" v-else>close</span>
-          webm/vp9
+          {{key}}
         </li>
       </ul>
       </div>
@@ -97,50 +29,51 @@
 
 export default {
   name: 'probe',
-  props: ['player', 'info'],
-  watch: {
-    info() {
-      console.log(this.info.manifest); // eslint-disable-line no-console
-    },
-  },
+  props: ['info'],
+  watch: {},
   computed: {
-    supportsDASH() {
-      return this.info && this.info.manifest.mpd;
+    supportedDrms() {
+      const drms = [
+        'com.widevine.alpha',
+        'com.apple.fps',
+        'com.microsoft.playready',
+      ];
+      const drm = this.info && this.info.drm;
+      if (drm) {
+        const filtered = Object.keys(drm)
+          .filter(key => drms.includes(key))
+          .reduce((obj, key) => {
+            obj[key] = drm[key]; // eslint-disable-line no-param-reassign
+            return obj;
+          }, {});
+        return filtered;
+      }
+      return {};
     },
-    supportsMSS() {
-      return this.info && this.info.manifest.ism;
-    },
-    supportsHLS() {
-      return this.info && this.info.manifest.m3u8;
-    },
-    supportsWidevine() {
-      return this.info && this.info.drm['com.widevine.alpha'];
-    },
-    supportsPlayready() {
-      return this.info && this.info.drm['com.microsoft.playready'];
-    },
-    supportsFairplay() {
-      return this.info && this.info.drm['com.apple.fps'];
-    },
-    supportsPrimetime() {
-      return this.info && this.info.drm['com.adobe.primetime'];
-    },
-    supportsMp4Avc1() {
-      return this.info && this.info.media['video/mp4; codecs="avc1.42E01E"'];
-    },
-    supportsMp4Hevc() {
-      return this.info
-        && (this.info.media['video/mp4; codecs="hev1.1.6.L93.90"']
-        || this.info.media['video/mp4; codecs="hvc1.1.6.L93.90"']);
-    },
-    supportsMp4Ec3() {
-      return this.info && this.info.media['audio/mp4; codecs="ec-3"'];
-    },
-    supportsWebmVp8() {
-      return this.info && this.info.media['video/webm; codecs="vp8"'];
-    },
-    supportsWebmVp9() {
-      return this.info && this.info.media['video/webm; codecs="vp9"'];
+    supportedMedia() {
+      const medias = [
+        'video/mp4; codecs="avc1.42E01E"',
+        'video/mp4; codecs="hev1.1.6.L93.90"',
+        'video/mp4; codecs="hvc1.1.6.L93.90"',
+        'audio/mp4; codecs="ac-3"',
+        'audio/mp4; codecs="ec-3"',
+        'audio/mp4; codecs="flac"',
+        'audio/mp4; codecs="mp4a.40.2"',
+        'audio/mp4; codecs="opus"',
+        'video/webm; codecs="vp8"',
+        'video/webm; codecs="vp9"',
+      ];
+      const media = this.info && this.info.media;
+      if (media) {
+        const filtered = Object.keys(media)
+          .filter(key => medias.includes(key))
+          .reduce((obj, key) => {
+            obj[key] = media[key]; // eslint-disable-line no-param-reassign
+            return obj;
+          }, {});
+        return filtered;
+      }
+      return {};
     },
   },
   methods: {
@@ -165,5 +98,10 @@ li span {
 
 li span.checked {
   color: green;
+}
+
+.codecs li {
+  display: inline-block;
+  width: 50%;
 }
 </style>
